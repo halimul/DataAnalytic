4a37d1d1639a1be2aa22fdbfa691f7e1d7131aee
halimul19 <82050586+halimul@users.noreply.github.com>
2023-01-25 18:17:01 +0700

    Add files via upload

---
+++ Emotion Detection/Model/realtime_vgg.py
@@ -0,0 +1,45 @@
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+from keras.applications import VGG16
+
+face_classifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('EmotionDetectionModelVGG16FineTune.h5')
+model_VGG = VGG16(weights='imagenet', include_top=False)
+
+class_labels=['Angry','Happy','Neutral','Sad','Surprise']
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=cap.read()
+    labels=[]
+    faces=face_classifier.detectMultiScale(frame,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        roi_gray=frame[y:y+h,x:x+w]
+        roi_gray=cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray])!=0:
+            roi=roi_gray.astype('float')/255.0
+            roi=img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            
+
+            preds=classifier.predict(roi)[0]
+            label=class_labels[preds.argmax()]
+            label_position=(x,y)
+            cv2.putText(frame, label, (x, y-5), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame,"Acc: " + str(np.amax(preds)), (x, y+20+h), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        else:
+            cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    
+    cv2.imshow('Program Deteksi Emosi',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
