769246819faed19f1c56d8deeea83360b4249ee1
halimul19 <82050586+halimul@users.noreply.github.com>
2023-03-12 00:29:10 +0700

    Add files via upload

---
+++ MySkill Practices & Project/SQL for Analysis/2-Data Preparation in Database/Studi Kasus Modul 1/Berkas 3 Studi Kasus Modul 1.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    siswa (id_siswa, nama_siswa, kelas_siswa)
+VALUES
+    ('#tuliskan_id_yang_ingin_dimasukkan', 
+    '#tuliskan_nama_yang_ingin_dimasukkan', 
+    '#tuliskan_nama_kelas_yang_ingin_dimasukkan');
+
+SELECT * FROM sekolah.siswa;
+
